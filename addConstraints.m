d7da4166d3559601321dcf3df543a439037f69cc
Bran <544524199@qq.com>
2015-08-13 11:47:53 -0700

    new constraints

--- addConstraints.m
+++ addConstraints.m
@@ -1,25 +1,50 @@
-function [ Sr,Sp ] = addConstraints(Sr,img_train,train_order )
+function [ Rp ] = addConstraints(Rr,img_train,train_order,type )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-[~,d]=size(Sr);
+if nargin<4
+    type=1;
+end
+[~,d]=size(Rr);
+    n=sum(train_order~=3);
+    if type==1
+        Rp=zeros(n,d);
+    else
+        Rp=zeros(50,d);
+    end
+    idx=find(train_order~=3);
 %d=2000;
-n=sum(train_order~=3);
-Sp=zeros(n,d);
-idx=find(train_order~=3);
-for i=1:length(idx)
-%     if train_order(idx(i))==1
-	Sp(i,img_train(idx(i),1)+d/2)=1;
-    Sp(i,img_train(idx(i),2)+d/2)=-1;
-%     end
-%     if train_order(idx(i))==2
-% 	Sp(i,img_train(idx(i),1)+d/2)=-1;
-%     Sp(i,img_train(idx(i),2)+d/2)=1;
-%     end
-%	if i>1
-%		[p,~]=sort([img_train(idx(i-1),2),img_train(idx(i),1)]);
-%		Sp(i,p(1)+d/2)=1;
-%		Sp(i,p(2)+d/2)=-1;
-%	end
+if type==1
+    for i=1:length(idx)
+    %     if train_order(idx(i))==1
+        Rp(i,img_train(idx(i),1)+d/2)=1;
+        Rp(i,img_train(idx(i),2)+d/2)=-1;
+    %     end
+    %     if train_order(idx(i))==2
+    % 	Sp(i,img_train(idx(i),1)+d/2)=-1;
+    %     Sp(i,img_train(idx(i),2)+d/2)=1;
+    %     end
+    %	if i>1
+    %		[p,~]=sort([img_train(idx(i-1),2),img_train(idx(i),1)]);
+    %		Sp(i,p(1)+d/2)=1;
+    %		Sp(i,p(2)+d/2)=-1;
+    %	end
+    end
+else
+    for i=1:50
+         if train_order(idx(i))==1
+        Rp(i,img_train(idx(i),1))=1;
+        Rp(i,img_train(idx(i),2)+d/2)=-1;
+         else
+    %     if train_order(idx(i))==2
+     	Rp(i,img_train(idx(i),1)+d/2)=-1;
+         Rp(i,img_train(idx(i),2))=1;
+         end
+    %	if i>1
+    %		[p,~]=sort([img_train(idx(i-1),2),img_train(idx(i),1)]);
+    %		Sp(i,p(1)+d/2)=1;
+    %		Sp(i,p(2)+d/2)=-1;
+    %	end
+    end
 end
 end
 
--- run.m
+++ run.m
@@ -1,6 +1,6 @@
 clear;
 %load 'smile.mat'
-%load 'teeth.mat'
+load 'teeth.mat'
 %load 'gistrgb.mat'
 %load 'blurgistrgb.mat'
 load 'regularmaskgistrgb.mat'
@@ -8,32 +8,38 @@ load 'regularmaskgistrgb.mat'
 %load './localsift/masksiftfeature.mat'
 %load 'convnetfeature.mat'
 %load 'graymaskconvnetfeature.mat'
-load 'mouthopen.mat'
-%feat=[convnetfeature(1:1000,:);graymaskconvnetfeature(1:1000,:)];
+load 'caffeconvnet.mat'
+load 'caffegrayconvnet.mat'
+%load 'mouthopen.mat'
+feat=[convnetfeature(1:1000,:);graymaskconvnetfeature(1:1000,:)];
 %feat=[convnetfeature(1:1000,:)];
-feat=[gistrgb(1:1000,:);regularmaskgistrgb(1:1000,:)];
+%feat=[gistrgb(1:1000,:);regularmaskgistrgb(1:1000,:)];
 s=[1];% 0.2 0.5 1 2 4 8 16 32 64 128 256 1000];
 [Om,Sm]=constructTraining(img_train,feat,train_order);
-[Sm,Sp]=addConstraints(Sm,img_train,train_order);
+[Rp]=addConstraints(Sm,img_train,train_order,2);
+%[Sp]=addConstraints(Sm,img_train,train_order,1);
 %Sp=[];
-Sp=sparse(Sp);
+%Sp=sparse(Sp);
+%Sm=[Sm;Sp];
+%Rp=sparse(Rp);
+%Om=[Om;Rp];
 Sm=sparse(Sm);
 Om=sparse(Om);
 for i=1:length(s)
-    c_s(1:(size(Sm,1)+size(Sp,1)))=s(i);
-    C_S=c_s';
-    c_o(1:size(Om,1))=s(i);
-    C_O=c_o';
-    wa=ranksvm_with_sim(feat,Om,[Sm;Sp],C_O,C_S);
-%     wa=ALTR_train(feat,Om,Sm,Sp,s(i),s(i),s(i));
-	wb=ALTR_train_v2(feat,Om,[Sm;Sp],s(i),s(i));
-%     wc=ALTR_train_v3(feat,Om,[Sm;Sp],s(i),s(i));
-    predictions=gistrgb*wa;
-    [AC]= test(predictions,img_test,test_order);
-	fprintf('C=%f, cvx_1 AC =%f\n',s(i),AC);
-	predictions=gistrgb*wb;
-    [AC]= test(predictions,img_test,test_order);
-    fprintf('C=%f,cvx_2 Ac=%f\n',s(i),AC);
+%     c_s(1:(size(Sm,1)+size(Sp,1)))=s(i);
+%     C_S=c_s';
+%     c_o(1:size(Om,1))=s(i);
+%     C_O=c_o';
+%    wa=ranksvm_with_sim(feat,Om,[Sm;Sp],C_O,C_S);
+     %wa=ALTR_train(feat,Om,Sm,Sp,s(i),s(i),s(i));
+    wb=ALTR_train_v4(feat,Om,Sm,Rp,s(i),s(i),1,2);
+    %wb=ALTR_train_v3(feat,Om,Sm,s(i),s(i));
+    predictions=convnetfeature*wb;
+     [AC]= test(predictions,img_test,test_order);
+ 	fprintf('C=%f, AC =%f\n',s(i),AC);
+% 	predictions=gistrgb*wb;
+%     [AC]= test(predictions,img_test,test_order);
+%     fprintf('C=%f,cvx_2 Ac=%f\n',s(i),AC);
 %     predictions=gistrgb*wc;
 %     [AC]= test(predictions,img_test,test_order);
 %     fprintf('C=%f,cvx_3 Ac=%f\n',s(i),AC);
